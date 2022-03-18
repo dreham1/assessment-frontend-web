fc9f4052bcc43105dfd07b33dffac60dff2d538a
Deondre Hambric <dhambric1@gmail.com>
2022-03-18 09:08:41 -0600

    first commit

---
+++ about.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form has been successfully submitted!!');
+}
+
+
+let form = document.querySelector('form#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+let img = document.querySelector('img');
+
+	img.addEventListener('mouseover', function(e){
+		alert('Give the user a compliment!')
+})
+	
