c1277a76fa1bd8c5ab993df43dc8c448338c9ec2
Deondre Hambric <dhambric1@gmail.com>
2022-03-18 11:07:51 -0600

    turning in

---
+++ profile.js
@@ -0,0 +1,18 @@
+
+
+let favcolorbTN = document.querySelector('#color');
+
+favcolorbTN.addEventListener('click', function(e){
+    alert('RED')
+    
+})
+
+let favPlaceBtn = document.querySelector('#place');
+    favPlaceBtn.addEventListener('click', function(e){
+        alert('OUTDOORS')
+})
+
+let favRit = document.querySelector('#ritual');
+favRit.addEventListener('click', function(e){
+    alert('EXERCISE')
+})
