1703122d093a9cc4de36114ce8ef15c7b309bd85
Deondre Hambric <dhambric1@gmail.com>
2022-03-21 07:28:30 -0600

    final answer

--- profile.js
+++ profile.js
@@ -1,18 +1,19 @@
 
 
-let favcolorbTN = document.querySelector('#color');
+const favcolorbTN = document.querySelector('#color');
 
 favcolorbTN.addEventListener('click', function(e){
     alert('RED')
     
 })
 
-let favPlaceBtn = document.querySelector('#place');
+const favPlaceBtn = document.querySelector('#place');
     favPlaceBtn.addEventListener('click', function(e){
         alert('OUTDOORS')
 })
 
-let favRit = document.querySelector('#ritual');
+const favRit = document.querySelector('#ritual');
 favRit.addEventListener('click', function(e){
     alert('EXERCISE')
 })    
+
